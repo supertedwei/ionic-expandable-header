c522a29e03869fc47249e0116ffee1454b14b113
Ted Wei <jdwei@hotmail.com>
2017-03-24 07:33:34 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ExpandableHeader } from '../components/expandable-header/expandable-header'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,7 +10,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ExpandableHeader
   ],
   imports: [
     IonicModule.forRoot(MyApp)
--- src/components/expandable-header/expandable-header.ts
+++ src/components/expandable-header/expandable-header.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-/*
-  Generated class for the ExpandableHeader component.
-
-  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
-  for more info on Angular 2 Components.
-*/
 @Component({
   selector: 'expandable-header',
   templateUrl: 'expandable-header.html'
 })
-export class ExpandableHeaderComponent {
+export class ExpandableHeader {
 
   text: string;
 
